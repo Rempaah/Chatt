587557660d7b90bd880d8d119b124ce64eba66f6
Rempaah <rmuhammadreff@gmail.com>
2023-11-05 13:22:15 +0700

    working login

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
+    session_start();
+    
+
     switch($_SERVER['REQUEST_URI'])
     {
         case '/':
+            if(isset($_SESSION["username"]))
+            {
+                header("location: chat");
+            }
             $file = fopen("src/Login.html", "r");
             $login = fread($file, filesize("src/Login.html"));
             echo $login;
             break;
         case '/chat':
+            if(!isset($_SESSION["username"]))
+            {
+                header("location: /chat");
+            }
             $file = fopen("src/Chat.html", "r");
             $chat = fread($file, filesize("src/Chat.html"));
             echo $chat;
--- php/Login.php
+++ php/Login.php
@@ -1,3 +1,31 @@
 <?php
+    session_start();
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "chatt";
+
+    $mysqli = new mysqli($host, $username, $password, $database);
+
+    if($mysqli->connect_error)
+    {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    $get = $mysqli->prepare("SELECT * FROM chatter WHERE username=?");
+    $get->bind_param("s", $_POST["username"]);
+    $get->execute();
+    $result = $get->get_result();
+    if($result->num_rows < 1)
+    {
+        $row = $result->fetch_assoc();
+        $insert = $mysqli->prepare("INSERT INTO chatter (username, color) VALUES (?, ?)");
+        $color = "hahah";
+        $insert->bind_param("ss", htmlspecialchars($_POST["username"]), $color);
+        $insert->execute();
+    }
+
+    $_SESSION["username"] = $_POST["username"];
     echo "success"
 ?>
